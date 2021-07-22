7739be0a2ec85333170988a4eaa85143eb5b3021
pearlin-varsha <83495317+pearlin-varsha@users.noreply.github.com>
2021-07-23 01:31:28 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+var cartValue =document.getElementById("cart-value");
+var cartButton=document.getElementById("cart");
+
+var book1Add= document.getElementById("book1");
+var book2Add= document.getElementById("book2");
+var book3Add= document.getElementById("book3");
+var book4Add= document.getElementById("book4");
+var book5Add= document.getElementById("book5");
+var book6Add= document.getElementById("book6");
+
+var game1Add= document.getElementById("game1");
+var game2Add= document.getElementById("game2");
+var game3Add= document.getElementById("game3");
+var game4Add= document.getElementById("game4");
+
+var craft1Add= document.getElementById("craft1");
+var craft2Add= document.getElementById("craft2");
+var craft3Add= document.getElementById("craft3");
+var craft4Add= document.getElementById("craft4");
+
+console.log(book1Add);
